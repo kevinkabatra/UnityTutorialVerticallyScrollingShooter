420c2ab7d99ed61988ddf26825e501973d94ade6
Kevin Kabatra <kevin@lsretail.com>
2020-12-19 03:28:15 +0000

    Adding intial work on ammunition

---
+++ Assets/UnityLayer/Player/Ship/Handler/ShipAmmunitionHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace UnityLayer.Player.Ship.Handler
+{
+    using Common;
+    using UnityEngine;
+
+    public class ShipAmmunitionHandler : Common<ShipAmmunitionHandler>
+    {
+        [SerializeField] private GameObject laserTest;
+
+        private void Start()
+        {
+            var testPosition = new Vector3
+            {
+                x = 8,
+                y = 6,
+                z = 0
+            };
+
+            var laserInstance = Instantiate<GameObject>(laserTest);
+            laserInstance.transform.position = testPosition;
+        }
+    }
+}
